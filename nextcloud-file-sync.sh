e2b34c231b746ae6eac0923c6896141521e7bf21
GAS85 <GAS85@users.noreply.github.com>
2017-02-02 08:58:50 +0100

    File sync
    Copied from here: https://forum.owncloud.org/viewtopic.php?f=29&t=23181&start=10

---
+++ nextcloud-file-sync.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COMMAND=/var/www/nextcloud/occ
+OPTIONS="files:scan --all"
+LOCKFILE=/tmp/nextcloud_file_scan
+LOGFILE=/var/www/nextcloud/data/nextcloud.log
+
+[ -f "$LOCKFILE" ] && exit
+
+touch $LOCKFILE
+echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Filescan +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+start=`date +%s`
+php $COMMAND $OPTIONS >> /var/log/next-cron.log
+end=`date +%s`
+echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+#echo -------------------------------------------------- >> $LOGFILE
+rm $LOCKFILE
