0b1311ae444b72a83da718127c7e0b23eef0f5ed
GAS85 <GAS85@users.noreply.github.com>
2017-03-23 08:59:52 +0100

    Update nextcloud-preview.sh
    add small workaround from here: https://help.nextcloud.com/t/clarity-on-the-crontab-settings-for-the-preview-generator-app/6144/39
    for gallery app.

--- nextcloud-preview.sh
+++ nextcloud-preview.sh
@@ -3,7 +3,8 @@
 COMMAND=/var/www/nextcloud/occ
 OPTIONS="preview:pre-generate"
 LOCKFILE=/tmp/nextcloud_preview
-LOGFILE=/var/www/nextcloud/data/nextcloud.log
+LOGFILE=/var/www/nextcloud/nextcloud.log
+NEXTPATH=/var/www/nextcloud/data/appdata_occoqcm6kiop/preview
 
 [ -f "$LOCKFILE" ] && exit
 
@@ -11,7 +12,19 @@ touch $LOCKFILE
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Preview generation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 start=`date +%s`
 php $COMMAND $OPTIONS >> /var/log/next-cron.log
+
 end=`date +%s`
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Preview generation Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 #echo -------------------------------------------------- >> $LOGFILE
+
+# Please use it if you have Gallery app installed
+#echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Gallery Preview creation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+#start=`date +%s`
+
+#cd $NEXTPATH
+#for i in $(find . -name "*-max.png") ; do [[ ! -f $(dirname $i)/$(basename ${i} -max.png).png ]] && ln -s $(dirname `readlink -e ${i}`)/$(basename $i) $(dirname `readlink -e $i`)/$(basename ${i} -max.png).png ; done
+
+#end=`date +%s`
+#echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Gallery preview cration Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+
 rm $LOCKFILE
