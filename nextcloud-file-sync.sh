f23a575be56487f0e5545211512513454f18e0d6
GAS85 <GAS85@users.noreply.github.com>
2017-03-24 08:50:40 +0100

    Update nextcloud-file-sync.sh

--- nextcloud-file-sync.sh
+++ nextcloud-file-sync.sh
@@ -4,14 +4,16 @@ COMMAND=/var/www/nextcloud/occ
 OPTIONS="files:scan --all"
 LOCKFILE=/tmp/nextcloud_file_scan
 LOGFILE=/var/www/nextcloud/nextcloud.log
+CRONLOGFILE=/var/log/next-cron.log
 
 [ -f "$LOCKFILE" ] && exit
 
 touch $LOCKFILE
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Filescan +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 start=`date +%s`
-php $COMMAND $OPTIONS >> /var/log/next-cron.log
+date >> $CRONLOGFILE
+php $COMMAND $OPTIONS >> $CRONLOGFILE
 end=`date +%s`
-echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Filescan Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Filescan Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 #echo -------------------------------------------------- >> $LOGFILE
 rm $LOCKFILE
