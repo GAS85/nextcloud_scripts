037c6acd3a58446619f789a7ee7ae1a39eb92db6
GAS85 <GAS85@users.noreply.github.com>
2017-03-24 08:43:28 +0100

    Update nextcloud-preview.sh

--- nextcloud-preview.sh
+++ nextcloud-preview.sh
@@ -4,17 +4,19 @@ COMMAND=/var/www/nextcloud/occ
 OPTIONS="preview:pre-generate"
 LOCKFILE=/tmp/nextcloud_preview
 LOGFILE=/var/www/nextcloud/nextcloud.log
-NEXTPATH=/var/www/nextcloud/data/appdata_occoqcm6kiop/preview
+CRONLOGFILE=/var/log/next-cron.log
+NEXTPATH=/var/www/nextcloud/data/appdata_<INSTANCE>/preview
 
 [ -f "$LOCKFILE" ] && exit
 
 touch $LOCKFILE
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Preview generation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 start=`date +%s`
-php $COMMAND $OPTIONS >> /var/log/next-cron.log
+date >> $CRONLOGFILE
+php $COMMAND $OPTIONS >> $CRONLOGFILE
 
 end=`date +%s`
-echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Preview generation Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Preview generation Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 #echo -------------------------------------------------- >> $LOGFILE
 
 # Please use it if you have Gallery app installed
@@ -22,9 +24,9 @@ echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Preview generation Compl
 #start=`date +%s`
 
 #cd $NEXTPATH
-#for i in $(find . -name "*-max.png") ; do [[ ! -f $(dirname $i)/$(basename ${i} -max.png).png ]] && ln -s $(dirname `readlink -e ${i}`)/$(basename $i) $(dirname `readlink -e $i`)/$(basename ${i} -max.png).png ; done
+#for i in $(find . -name "*-max.png") ; do [[ ! -f $(dirname $i)/$(basename ${i} -max.png).png ]] && ln -s $(dirname `readlink -e ${i}`)/$(basename $i) $(dirname `readlink -e $i`)/$(basename ${i} -max.png).png ; done >> $CRONLOGFILE
 
 #end=`date +%s`
-#echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Gallery preview cration Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+#echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Gallery preview creation Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 
 rm $LOCKFILE
