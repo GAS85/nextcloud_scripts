9186788a473b60887e10fd2d30ddb36710bf03a6
GAS85 <GAS85@users.noreply.github.com>
2017-07-14 10:25:42 +0200

    Update backupzipper.sh

--- backupzipper.sh
+++ backupzipper.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-WORKINGDIR=/media/raid/backups/mysql
+WORKINGDIR=/media/backup
 LOCKFILE=/tmp/zipping
 BACKUPNAME=backup-$(date +"%Y-%m-%d").zip
+recipients="user1@gmail.com,user2@gmail.com,user3@gmail.com"
+subject="Backup was done"
+from="noreplay@nobody.net"
 
 [ -f "$LOCKFILE" ] && exit
 
@@ -10,20 +13,27 @@ touch $LOCKFILE
 
 #Random password
 choose() { echo ${1:RANDOM%${#1}:1} $RANDOM; }
-pass="$({ choose '!_@'
+pass="$({ choose '!@#$%^\&'
   choose '0123456789'
   choose 'abcdefghijklmnopqrstuvwxyz'
   choose 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  for i in $( seq 1 $(( 4 + RANDOM % 16 ))  
+  for i in $( seq 1 $(( 4 + RANDOM % 16 )) )
      do
         choose '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
      done
  } | sort -R | awk '{printf "%s",$1}')"
- 
-#echo $pass
 
 cd $WORKINGDIR
 zip --password $pass $BACKUPNAME *.sql.gz
+
+#send email with password
+/usr/sbin/sendmail "$recipients" <<EOF
+subject:$subject
+from:$from
+The backup was created with password: $pass
+
+Have a nice day!
+EOF
 
 rm $LOCKFILE
 exit
