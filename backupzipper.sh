7c26386b570541fa010ae5edcb45e7675e9fc342
GAS85 <GAS85@users.noreply.github.com>
2017-10-20 10:56:37 +0200

    Update backupzipper.sh
    Go to loops for more dynamic and less code

--- backupzipper.sh
+++ backupzipper.sh
@@ -2,15 +2,19 @@
 
 WORKINGDIR=/media/backup
 LOCKFILE=/tmp/zipping
+EMAILFILE=/tmp/zipping.email
 BACKUPNAME=backup-$(date +"%Y-%m-%d").zip
-ATTACHDIR=/home/gas/samba/cacti/graphs
+ATTACHDIR=/files/to/be/attached
 recipients="user1@gmail.com,user2@gmail.com,user3@gmail.com"
 subject="MySQL backup was done"
 from="noreplay@nobody.net"
+#megapass="xxxxxxx"
+#megalogin="zzzzzzz"
 
 [ -f "$LOCKFILE" ] && exit
 
 touch $LOCKFILE
+touch $EMAILFILE
 
 #Random password
 choose() { echo ${1:RANDOM%${#1}:1} $RANDOM; }
@@ -24,137 +28,83 @@ pass="$({ choose '!@#$%^\&'
      done
  } | sort -R | awk '{printf "%s",$1}')"
 
-#zipping with password from above
 cd $WORKINGDIR
-zip --password $pass $BACKUPNAME *.sql.gz
 
-#Upload to Mega /Backup folder
-megaput --no-progress --path /Root/Backup $BACKUPNAME
+#zipping with password from above
+zip --password $pass $BACKUPNAME *.sql.gz 1>$LOCKFILE
 
-#send email with password
+#Upload to Mega
+megaput --no-progress --path /Root/Backup $BACKUPNAME >>$LOCKFILE
+#megaput -u $megalogin -p $megapass --no-progress --path /Root/Backup $BACKUPNAME 2>>$LOCKFILE
+#megaput -u $megalogin -p $megapass --path /Root/Backup $BACKUPNAME 2>>$LOCKFILE
 
-export ATTACH=$ATTACHDIR/thumb_1.png
-export ATTACH1=$ATTACHDIR/thumb_2.png
-export ATTACH2=$ATTACHDIR/thumb_3.png
-export ATTACH3=$ATTACHDIR/thumb_4.png
-export ATTACH4=$ATTACHDIR/thumb_7.png
-export ATTACH5=$ATTACHDIR/thumb_8.png
-export ATTACH6=$ATTACHDIR/thumb_11.png
-export ATTACH7=$ATTACHDIR/thumb_15.png
-export ATTACH8=$ATTACHDIR/thumb_18.png
-export ATTACH9=$ATTACHDIR/thumb_21.png
-export ATTACH10=$ATTACHDIR/thumb_22.png
-export BODY="The backup was created with password: $pass Have a nice day and check some statistic."
-(
-echo "To: $recipients"
-echo "FROM: $from"
-echo "Subject: $subject"
-echo "MIME-Version: 1.0"
-echo 'Content-Type: multipart/mixed; boundary="-q1w2e3r4t5"'
-echo
-echo '---q1w2e3r4t5'
-echo "Content-Type: text/html"
-echo "Content-Disposition: inline"
-echo
-echo $BODY
-megadf -h
-echo
+#Email Header
+echo "To: $recipients" > $EMAILFILE
+echo "FROM: $from" >> $EMAILFILE
+echo "Subject: $subject" >> $EMAILFILE
+echo "MIME-Version: 1.0" >> $EMAILFILE
+echo 'Content-Type: multipart/mixed; boundary="-q1w2e3r4t5"' >> $EMAILFILE
+echo >> $EMAILFILE
+echo '---q1w2e3r4t5' >> $EMAILFILE
+echo "Content-Type: text/html" >> $EMAILFILE
+echo "Content-Disposition: inline" >> $EMAILFILE
+echo "" >> $EMAILFILE
+echo "The backup was created with password: $pass" >> $EMAILFILE
+echo "Have a nice day and check some statistic." >> $EMAILFILE
+#echo "$(megadf -u $megalogin -p $megapass -h)." >> $EMAILFILE
+echo "$(megadf -h)." >> $EMAILFILE
+echo "Other info: $(cat $LOCKFILE)." >> $EMAILFILE
+echo "" >> $EMAILFILE
+
 #
 # So make man inline file with base64 or uuencode. uuencode will not be read by some programms
 #
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH)''
-#echo "Content-Transfer-Encoding: uuencode"
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH)''
-echo "Content-ID: <$(basename $ATTACH)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH
+#echo '---q1w2e3r4t5'
+#echo 'Content-Type: image/png; name='$(basename $ATTACH)''
+##echo "Content-Transfer-Encoding: uuencode"
+#echo "Content-Transfer-Encoding: base64"
+#echo 'Content-Disposition: inline; filename='$(basename $ATTACH)''
+#echo "Content-ID: <$(basename $ATTACH)>"
+#echo '---q1w2e3r4t5--'
+#base64 $ATTACH
 #uuencode $ATTACH $(basename $ATTACH)
 #
 # #######################
 #
 # so make man attachment file with uuencode or base64.
 #
+##echo '---q1w2e3r4t5'
+##echo 'Content-Type: application; name="'$(basename $ATTACH)'"'
+##echo "Content-Transfer-Encoding: uuencode"
+###echo "Content-Transfer-Encoding: base64"
+##echo 'Content-Disposition: attachment; filename="'$(basename $ATTACH)'"'
+##echo '---q1w2e3r4t5--'
+###base64 $ATTACH
+##uuencode $ATTACH $(basename $ATTACH)
 #echo '---q1w2e3r4t5'
-#echo 'Content-Type: application; name="'$(basename $ATTACH)'"'
-#echo "Content-Transfer-Encoding: uuencode"
-##echo "Content-Transfer-Encoding: base64"
-#echo 'Content-Disposition: attachment; filename="'$(basename $ATTACH)'"'
+#echo 'Content-Type: image/png; name='$(basename $ATTACH1)''
+#echo "Content-Transfer-Encoding: base64"
+#echo 'Content-Disposition: inline; filename='$(basename $ATTACH1)''
+#echo "Content-ID: <$(basename $ATTACH1)>"
 #echo '---q1w2e3r4t5--'
-##base64 $ATTACH
-#uuencode $ATTACH $(basename $ATTACH)
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH1)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH1)''
-echo "Content-ID: <$(basename $ATTACH1)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH1
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH2)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH2)''
-echo "Content-ID: <$(basename $ATTACH2)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH2
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH3)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH3)''
-echo "Content-ID: <$(basename $ATTACH3)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH3
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH4)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH4)''
-echo "Content-ID: <$(basename $ATTACH4)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH4
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH5)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH5)''
-echo "Content-ID: <$(basename $ATTACH5)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH5
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH6)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH6)''
-echo "Content-ID: <$(basename $ATTACH6)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH6
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH7)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH7)''
-echo "Content-ID: <$(basename $ATTACH7)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH7
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH8)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH8)''
-echo "Content-ID: <$(basename $ATTACH8)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH8
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH9)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH9)''
-echo "Content-ID: <$(basename $ATTACH9)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH9
-echo '---q1w2e3r4t5'
-echo 'Content-Type: image/png; name='$(basename $ATTACH10)''
-echo "Content-Transfer-Encoding: base64"
-echo 'Content-Disposition: inline; filename='$(basename $ATTACH10)''
-echo "Content-ID: <$(basename $ATTACH10)>"
-echo '---q1w2e3r4t5--'
-base64 $ATTACH10
-) | /usr/sbin/sendmail $recipients
+
+for entry in "$ATTACHDIR"/graph_*_1.png
+do
+	export ATTACH=$entry
+    
+	echo '---q1w2e3r4t5' >> $EMAILFILE
+	echo 'Content-Type: image/png; name='$(basename $ATTACH)'' >> $EMAILFILE
+	echo "Content-Transfer-Encoding: base64" >> $EMAILFILE
+	echo 'Content-Disposition: inline; filename='$(basename $ATTACH)'' >> $EMAILFILE
+	echo "Content-ID: <$(basename $ATTACH)>" >> $EMAILFILE
+	echo '---q1w2e3r4t5--' >> $EMAILFILE
+	base64 $ATTACH >> $EMAILFILE
+    
+done
+
+#send email with password
+cat $EMAILFILE | /usr/sbin/sendmail $recipients
 
 rm $LOCKFILE
-exit
+rm $EMAILFILE
+exit 0
