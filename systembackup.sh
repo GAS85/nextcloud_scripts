7803e23c1b0224a29e99d7ee8e4d8983b9bf0c49
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-02-23 11:06:20 +0100

    Update systembackup.sh
    Password was enhanced with SHA1 to be longer.
    files cleanup now automated independent of file name

--- systembackup.sh
+++ systembackup.sh
@@ -7,17 +7,18 @@ subject="System backup was done"
 from="noreplay@nobody.net"
 megapass="xxxxxx"
 megalogin="yyyyyy"
+BACKUPNAME=sys-backup-$(date +"%Y-%m-%d").tar.gz.gpg
 
 ###Please do not edit following 3 Lines:
 LOCKFILE=/tmp/sysbackup
 EMAILFILE=/tmp/sysbackup.mail
-BACKUPNAME=sys-backup-$(date +"%Y-%m-%d").tar.gz.gpg
+ToFind="$(echo $BACKUPNAME | cut -c1-5)*$(echo $BACKUPNAME | sed 's/.*\(...\)/\1/')"
 
 [ -f "$LOCKFILE" ] && exit
 
 #Check if Working dir exist
 if [ ! -d "$WORKINGDIR" ]; then
-	echo "Directory $WORKINGDIR does not exist"
+        echo "Directory $WORKINGDIR does not exist"
     exit 1
 fi
 
@@ -39,10 +40,13 @@ pass="$({
      done
  } | sort -R | awk '{printf "%s",$1}')"
 
+#increase password length with sha1
+pass="$(echo "${pass}$(sha1sum <<< $pass$(date) | awk '{printf "%s",$1}')" | grep -o . | shuf | tr -d "\n")"
+
 cd $WORKINGDIR
 
 #Do System backup
-tar -cvp --exclude=$WORKINGDIR --exclude=/proc --exclude=/tmp --exclude=/mnt --exclude=/dev --exclude=/sys --exclude=/run --exclude=/media --exclude=/var/log --exclude=/var/cache/apt/archives --exclude=/var/www/nextcloud/data --exclude=/usr/src/linux-headers* --one-file-system / | gpg --passphrase "$pass" --symmetric --no-tty -o $BACKUPNAME 2>>$LOCKFILE
+tar -cvp --exclude=$WORKINGDIR --exclude=/home/gas/samba --exclude=/proc --exclude=/tmp --exclude=/mnt --exclude=/dev --exclude=/sys$
 
 middle=`date +%s`
 
@@ -51,9 +55,8 @@ megaput -u $megalogin -p $megapass --path /Root/Backup $BACKUPNAME 2>>$LOCKFILE
 
 #delete local old backups
 # +15 is older than 15 days
-#echo $ToFind
-#find $ToFind -mtime +15 -exec rm {} \; 2>>$LOCKFILE
-find sys*gpg -mtime +15 -exec rm {} \; 2>>$LOCKFILE
+find "$ToFind" -mtime +15 -exec rm {} \; 2>>$LOCKFILE
+#find sys*gpg -mtime +15 -exec rm {} \; 2>>$LOCKFILE
 
 end=`date +%s`
 
@@ -69,7 +72,7 @@ echo "Content-Type: text/html" >> $EMAILFILE
 echo "Content-Disposition: inline" >> $EMAILFILE
 echo "" >> $EMAILFILE
 echo 'The backup was created with password: '"'$pass'"'<br>' >> $EMAILFILE
-echo "It took `expr $middle - $start`s to create and `expr $end - $middle`s to upload backup file, or `expr $end - $start`s at all.<br>" >> $EMAILFILE
+echo "It took `expr $middle - $start`s to create and `expr $end - $middle`s to upload backup file, or `expr $end - $start`s at all.<$
 echo "Have a nice day and check some statistic.<br>">> $EMAILFILE
 echo "<br>">> $EMAILFILE
 echo "Backup size: $(du -h $BACKUPNAME | awk '{printf "%s",$1}').<br>" >> $EMAILFILE
