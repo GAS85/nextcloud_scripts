35686792ad51595b455603d33a2185bb0bcf1812
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-04-10 12:21:21 +0200

    Update nextcloud-preview.sh

--- nextcloud-preview.sh
+++ nextcloud-preview.sh
@@ -9,7 +9,13 @@ LOGFILE=/var/www/nextcloud/data/nextcloud.log
 CRONLOGFILE=/var/log/next-cron.log
 NEXTPATH=/var/www/nextcloud/data/appdata_<INSTANCE>/preview
 
-[ -f "$LOCKFILE" ] && exit
+#[ -f "$LOCKFILE" ] && exit
+
+if [ -f "$LOCKFILE" ]; then
+	# Remove lock file if script fails last time and did not run longer than 10 days due to lock file.
+	find "$LOCKFILE" -mtime +10 -type f -delete
+  exit 1
+fi
 
 touch $LOCKFILE
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Preview generation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
