068bd494c52339205520a3af5d441a21128e9aff
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-08-22 12:18:37 +0200

    Update nextcloud-file-sync.sh
    Make it working without LOGs.
    Add -all option to rescan everything.

--- nextcloud-file-sync.sh
+++ nextcloud-file-sync.sh
@@ -2,20 +2,56 @@
 
 # By Georgiy Sitnikov.
 #
-# Will do external shares rescan for nextcloud and put execution information in NC log
+# Will do external ONLY shares rescan for nextcloud and put execution information in NC log.
+# If you would like to perform WHOLE nextcloud rescan, please add -all to command, e.g.:
+# ./nextcloud-file-sync.sh -all
 #
 # AS-IS without any warranty
 
+# Adjust to your NC installation
+	# Your NC OCC Command path
 COMMAND=/var/www/nextcloud/occ
+	# Your NC log file path
+LOGFILE=/var/www/nextcloud/data/nextcloud.log
+	# Your log file path for other output if needed
+CRONLOGFILE=/var/log/next-cron.log
+	# If you want to perform cache cleanup, please change CACHE value to 1
+CACHE=0
+
+
+# Live it like this
 OPTIONS="files:scan"
 LOCKFILE=/tmp/nextcloud_file_scan
-LOGFILE=/var/www/nextcloud/data/nextcloud.log
-CRONLOGFILE=/var/log/next-cron.log
+KEY="$1"
 
 if [ -f "$LOCKFILE" ]; then
 	# Remove lock file if script fails last time and did not run longer than 10 days due to lock file.
 	find "$LOCKFILE" -mtime +10 -type f -delete
-    exit 1
+	exit 1
+fi
+
+# Check if OCC is reacheble
+if [ ! -x "$COMMAND" ]; then
+	echo "ERROR - Command $COMMAND not found. Make sure if path is corrct and user has right to execute it."
+	exit 1
+fi
+
+# Check if php is executable
+if [ ! -x php ]; then
+	echo "ERROR - PHP not found."
+	exit 1
+fi
+
+# Check if NC Log file is writable
+if [ ! -w "$LOGFILE" ]; then
+	echo "WARNING - could not write to Log file $LOGFILE, will drop log messages. Is User Correct?"
+	LOGFILE=/dev/null
+fi
+
+# Check if CRON Log file is writable
+if [ ! -w "$CRONLOGFILE" ]; then
+	echo "WARNING - could not write to Log file $CRONLOGFILE, will drop log messages. Is User Correct?"
+	CRONLOGFILE=/dev/null
 fi
 
 touch $LOCKFILE
@@ -24,8 +60,10 @@ start=`date +%s`
 date >> $CRONLOGFILE
 
 # scan all files of all users (Takes ages)
-#php $COMMAND $OPTIONS --all >> $CRONLOGFILE
 
+if [ "$KEY" == "-all" ]; then
+	php $COMMAND $OPTIONS --all >> $CRONLOGFILE
+fi
 
 # scan all files of selected users
 #php $COMMAND $OPTIONS [user_id] >> $CRONLOGFILE
@@ -41,31 +79,30 @@ date >> $CRONLOGFILE
 #   or
 # "user_id/files/mount_name/path"
 
-# get ALL external mounting points and users
-php $COMMAND files_external:list | awk -F'|' '{print $8"/files"$3}'| tail -n +4 | head -n -1 | awk '{gsub(/ /, "", $0); print}' > $LOCKFILE
-
-## Choice one
-# rescan all shares
-cat $LOCKFILE | while read line ; do php $COMMAND $OPTIONS --path="$line" >> $CRONLOGFILE ; done
-# rescan all files for specific "user1"
-#php $COMMAND $OPTIONS user1 >> $CRONLOGFILE
-# rescan WHOLE cloud
-#php $COMMAND $OPTIONS --all >> $CRONLOGFILE
-
+if [ "$KEY" != "-all" ]; then
+	# get ALL external mounting points and users
+	php $COMMAND files_external:list | awk -F'|' '{print $8"/files"$3}'| tail -n +4 | head -n -1 | awk '{gsub(/ /, "", $0); print}' > $LOCKFILE
+		
+	# rescan all shares
+	cat $LOCKFILE | while read line ; do php $COMMAND $OPTIONS --path="$line" >> $CRONLOGFILE ; done
+fi
 
 end=`date +%s`
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Filescan Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 
 # OPTIONAL
 ### Start Cache cleanup
-echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Files Cache cleanup +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
-start=`date +%s`
-date >> $CRONLOGFILE
-php $COMMAND files:cleanup >> $CRONLOGFILE
-end=`date +%s`
-echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Files Cache cleanup Completed. Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
-### FINISCH Cache cleanup
 
+if [ "$CACHE" -eq "1" ]; then
+	echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Files Cache cleanup +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+start=`date +%s`
+	date >> $CRONLOGFILE
+	php $COMMAND files:cleanup >> $CRONLOGFILE
+	end=`date +%s`
+	echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Files Cache cleanup Completed. Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
+fi
+### FINISCH Cache cleanup
 
 #echo -------------------------------------------------- >> $LOGFILE
 rm $LOCKFILE
+exit 0
