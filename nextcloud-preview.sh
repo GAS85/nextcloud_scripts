4cc0ad7bf1e8a398ae7d7c6ae115891ee6491bd8
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-11-07 14:27:47 +0100

    Update nextcloud-preview.sh
    seems needed, as soon as preview:generate-all does not check if it is already run.

--- nextcloud-preview.sh
+++ nextcloud-preview.sh
@@ -1,28 +1,45 @@
 #!/bin/bash
 
+# Path to your occ command.
+# E.g. /var/www/nextcloud/occ
 COMMAND=/var/www/nextcloud/occ
+
+# # Possible options:
+#  preview:pre-generate - to generate preview to all NEW files
+#  preview:generate-all - to rescan whole system and generate previews 
 OPTIONS="preview:pre-generate"
-# use to see all touched files
-#OPTIONS="preview:pre-generate -vvv"
-LOCKFILE=/tmp/nextcloud_preview
+#OPTIONS="preview:generate-all"
+
+# # use to see all touched files
+# Possible values (e.g. Debug level) -v, -vv, -vvv.
+#DEBUG="-vvv"
+
+# Path to NC log file
 LOGFILE=/var/www/nextcloud/data/nextcloud.log
+
+# Path to log file for this script
 CRONLOGFILE=/var/log/next-cron.log
-NEXTPATH=/var/www/nextcloud/data/appdata_<INSTANCE>/preview
+
+LOCKFILE=/tmp/nextcloud_preview
 
 if [ -f "$LOCKFILE" ]; then
-	# Remove lock file if script fails last time and did not run longer than 10 days due to lock file.
+	# Remove lock file if script fails last time and did not run more then 10 days due to lock file.
 	find "$LOCKFILE" -mtime +10 -type f -delete
-  exit 1
+	echo "WARNING - Other instance is still active, exiting." >> $CRONLOGFILE
+	exit 1
 fi
 
 touch $LOCKFILE
+
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Preview generation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 start=`date +%s`
 date >> $CRONLOGFILE
 
-php $COMMAND $OPTIONS >> $CRONLOGFILE
+php $COMMAND $OPTIONS $DEBUG >> $CRONLOGFILE
 
 end=`date +%s`
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Preview generation Completed.    Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 
 rm $LOCKFILE
+
+exit 0
