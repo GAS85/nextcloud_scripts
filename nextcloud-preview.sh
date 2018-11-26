9bda2773322152c6857880d0914bb3093a235a08
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2018-11-26 09:50:27 +0100

    Update nextcloud-preview.sh

--- nextcloud-preview.sh
+++ nextcloud-preview.sh
@@ -21,6 +21,9 @@ LOGFILE=/var/www/nextcloud/data/nextcloud.log
 # Path to log file for this script
 CRONLOGFILE=/var/log/next-cron.log
 
+# Your PHP location if differnt
+PHP=/usr/bin/php
+
 ### Please do not touch under this line ###
 
 LOCKFILE=/tmp/nextcloud_preview
@@ -32,18 +35,44 @@ if [ -f "$LOCKFILE" ]; then
 	exit 1
 fi
 
-touch $LOCKFILE
-
-if [ ! -f "$CRONLOGFILE" ]; then
-	# Redirect output if $CRONLOGFILE not found
-	CRONLOGFILE="/dev/null"
+# Check if OCC is reacheble
+if [ ! -w "$COMMAND" ]; then
+	echo "ERROR - Command $COMMAND not found. Make sure taht path is corrct."
+	exit 1
+else
+	if [ "$EUID" -ne "$(stat -c %u $COMMAND)" ]; then
+		echo "ERROR - Command $COMMAND not executable for current user.
+	Make sure that user has right to execute it.
+	Script must be executed as $(stat -c %U $COMMAND)."
+		exit 1
+	fi
 fi
+
+# Check if php is executable
+if [ ! -x "$PHP" ]; then
+	echo "ERROR - PHP not found, or not executable."
+	exit 1
+fi
+
+# Check if NC Log file is writable
+if [ ! -w "$LOGFILE" ]; then
+	echo "WARNING - could not write to Log file $LOGFILE, will drop log messages. Is User Correct? Current log file owener is $(stat -c %U $LOGFILE)"
+	LOGFILE=/dev/null
+fi
+
+# Check if CRON Log file is writable
+if [ ! -w "$CRONLOGFILE" ]; then
+	echo "WARNING - could not write to Log file $CRONLOGFILE, will drop log messages. Is User Correct? Current log file owener is $(stat -c %U $CRONLOGFILE)"
+	CRONLOGFILE=/dev/null
+fi
+
+touch $LOCKFILE
 
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Starting Cron Preview generation +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
 start=`date +%s`
 date >> $CRONLOGFILE
 
-php $COMMAND $OPTIONS $DEBUG >> $CRONLOGFILE
+$PHP $COMMAND $OPTIONS $DEBUG >> $CRONLOGFILE
 
 end=`date +%s`
 echo \{\"app\":\"$COMMAND $OPTIONS\",\"message\":\""+++ Cron Preview generation Completed. Time: `expr $end - $start`s +++"\",\"level\":1,\"time\":\"`date "+%Y-%m-%dT%H:%M:%S%:z"`\"\} >> $LOGFILE
