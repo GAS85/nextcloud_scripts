5e9bfc5dd13ff177e9a198523e870677b12197ac
Georgiy Sitnikov <GAS85@users.noreply.github.com>
2019-03-19 10:03:07 +0100

    Update and rename nextcloud-usage_report.sh to nextcloud-usage-report.sh
    Added central Config

--- nextcloud-usage-report.sh
+++ nextcloud-usage-report.sh
@@ -15,12 +15,15 @@ COMMAND=/var/www/nextcloud/occ
 OPTIONS="usage-report:generate"
 LOCKFILE=/tmp/nextcloud_usagereport
 TMPFILE=/tmp/nextcloud_usagereport_tmp
+PHP=/usr/bin/php
+
+. nextcloud-scripts-config.conf
 
 [ -f "$LOCKFILE" ] && exit
 touch $LOCKFILE
 
 #get usage imnformation
-php $COMMAND $OPTIONS $1 > $LOCKFILE
+$PHP $COMMAND $OPTIONS $1 > $LOCKFILE
 
 #generate log in Cacti format
 awk -F, '{print $1"storage_all:"$3" " $1"storage_used:"$4" " $1"files_all:"$5" " $1"shares_new:"$6" " $1"files_new:"$7" " $1"files_read:"$8" "}' $LOCKFILE | sed 's/"//g' | sed ':a;N;$!ba;s/\n/ /g' >> $TMPFILE
