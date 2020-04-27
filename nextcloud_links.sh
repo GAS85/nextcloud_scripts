bf5d513c36a890e56c54f70a7460ab98021df729
Georgiy Sitnikov <georgiy.sitnikov@gmail.com>
2020-04-27 16:13:38 +0200

    Added Links Bot for a Talk

---
+++ nextcloud_links.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+ 
+# By Georgiy Sitnikov.
+#
+# AS-IS without any warranty
+# 
+# To added to Nextcloud please execute:
+# sudo -u www-data php /var/www/nextcloud/occ talk:command:add links links "/usr/local/bin/nextcloud_links.sh {ARGUMENTS} {ROOM} {USER}" 2 3
+# More infor under https://nextcloud-talk.readthedocs.io/en/latest/commands/
+
+while test $# -gt 0; do
+	case "$1" in
+		--help)
+			echo "/links - A Nextcloud Talk chat wrapper for important links"
+			echo " "
+			echo "Simple execution: /links"
+			echo "Complex execution: /links wiki"
+			exit 0
+			;;
+		*)
+	break
+	;;
+esac
+done
+
+list="/usr/local/bin/nextcloud_links_list"
+
+echo "Hey, $3 here is something useful for you:"
+if [ "$1" = "" ]; then
+	#will display whole list because nothing specifyed
+	cat $list
+else
+	#will display exact match, or an error message
+	if ! grep $1 $list; then
+		echo "Hmmm, nothing was found"
+	fi
+fi
+
+exit 0
