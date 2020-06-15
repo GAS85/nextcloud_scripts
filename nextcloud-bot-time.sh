60b93587b31b455bc7e17e27f494a233e86a3302
Georgiy Sitnikov <georgiy.sitnikov@gmail.com>
2020-06-15 15:44:58 +0200

    Some updates

--- nextcloud-bot-time.sh
+++ nextcloud-bot-time.sh
@@ -8,6 +8,8 @@
 # sudo -u www-data php /var/www/nextcloud/occ talk:command:add time time "/usr/local/bin/nextcloud-bot-time.sh {ARGUMENTS}" 1 3
 # More infor under https://nextcloud-talk.readthedocs.io/en/latest/commands/
 
+maxDelay=5
+
 temp="/tmp/nextcloud-bot-time-$(date +"%m-%d-%N")"
 
 while test $# -gt 0; do
@@ -43,40 +45,54 @@ done
 
 apiCall () {
  
-	#curl -s -m 5 "http://worldtimeapi.org/api/timezone/"$call | sed 's/,/\n/g' | sed 's/"//g' | sed 's/{\|}//g' | sed 's/\]//g' | sed 's/\[//g' > $temp
-
 	if [ -z "$call" ]; then
 
-		curl -s -m 5 "http://worldtimeapi.org/api/timezone.txt" > $temp
+		curl -s -m $maxDelay "http://worldtimeapi.org/api/timezone.txt" > $temp
 
 	else
 
-		curl -s -m 5 "http://worldtimeapi.org/api/timezone/"$call.txt > $temp
+		curl -s -m $maxDelay "http://worldtimeapi.org/api/timezone/"$call.txt > $temp
 
 	fi
 
-#	if grep "Error" $temp; then
+}
 
-#		echo "Uuups, some error is here. Please try again later."
-#		echo "For help, please, type /time --help"
-#		rm $temp
-#		exit 0
+APInewTry () {
 
-#	fi
+	newTry=$call
+	call=""
+	apiCall
+	call=$(grep "$newTry" $temp | head -n 1)
+
+	if [ ! -z "$call" ]; then
+
+		echo "Did you mean $call?"
+		apiCall
+		echo "Current local time in $call: "$(grep datetime $temp | tail -c33 | head -c10), $(grep datetime $temp | tail -c+21 | head -c8) $(grep abbreviation $temp | tail -c+14)
+
+	fi
+
+}
+
+checkForErrors () {
 
 	if [ "$(cat $temp| head -c 5)" = "Error" ]; then
 
 		awk '{ $1 = "Uuups, some error is here:"; print $0 }' $temp
 
-		newTry=$(curl -s -m 5 "http://worldtimeapi.org/api/timezone.txt" | grep "$call" | head -n 1)
+		APInewTry
 
-		if [ ! -z "$newTry" ]; then
-        
-			echo "Did you mean $newTry?"
-        
-		fi
-
+		echo " "
 		echo "For help, please, type /time --help"
+		rm $temp
+		exit 0
+
+	fi
+
+	# Added check if List returned
+	if [ "$(grep datetime $temp | tail -c33 | head -c10)" = "" ] && [ "$first" != "--list" ]; then
+
+		APInewTry
 		rm $temp
 		exit 0
 
@@ -88,11 +104,22 @@ apiCall () {
 first="$(echo $1 | head -c 6)"
 rest="$(echo $1 | tail -c +8)"
 
+#Added input Validator
+#https://stackoverflow.com/questions/36926999/removing-all-special-characters-from-a-string-in-bash
+validInput="$(echo "$rest" | sed 's/[^a-z A-Z 0-9]//g')"
+if [ "$validInput" = "" ] && [ ! -z "$rest" ]; then
+
+	echo "Seems you use only Special Characters, currently only a-z, A-Z and digits are supported"
+	exit 0
+
+fi
+
 if [ "$first" = "--list" ]; then
 
 	apiCall
+	checkForErrors
 
-	if [ -z "$rest" ]; then
+	if [ -z "$validInput" ]; then
 
 		echo "Here you can find whole list of Timezones:"
 		cat $temp
@@ -101,10 +128,34 @@ if [ "$first" = "--list" ]; then
 
 		echo "Searching for your input in Timezones:"
 
-		if ! grep "$rest" $temp; then
-			echo "Hmmm, nothing was found"
-			echo "Try /time --list to see all Locations, or /time --help for help"
-		fi
+		#Check if multiple words are separated by spaces
+		case "$validInput" in
+
+			*\ * )
+
+				multipleInput=$(echo "$validInput" | tr ' ' '|')
+				#will display exact mutliple match, or an error message
+				if ! grep -E "$multipleInput" "$temp"; then
+
+					echo "Hmmm, nothing was found"
+					echo "Try /time --list to see all Locations, or /time --help for help"
+
+				fi
+
+			;;
+
+			*)
+
+				#will display exact match, or an error message
+				if ! grep "$validInput" "$temp"; then
+
+					echo "Hmmm, nothing was found"
+					echo "Try /time --list to see all Locations, or /time --help for help"
+
+				fi
+
+			;;
+		esac
 
 	fi
 
@@ -122,8 +173,9 @@ fi
 
 call=$1
 apiCall
- 
-echo "Current local time in $1: "$(grep datetime $temp | tail -c33 | head -c10), $(grep datetime $temp | tail -c+21 | head -c8) $(grep abbreviation $temp | tail -c+14)""
+checkForErrors
+
+echo "Current local time in $1: "$(grep datetime $temp | tail -c33 | head -c10), $(grep datetime $temp | tail -c+21 | head -c8) $(grep abbreviation $temp | tail -c+14)
 
 rm $temp
 exit 0
